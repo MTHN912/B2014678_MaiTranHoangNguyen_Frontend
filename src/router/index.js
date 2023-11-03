d38701b15168c890348da14d89577d9cd065588d
MTHN912 <mthnguyen912na@gmail.com>
2023-11-03 07:36:36 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,31 @@
-import ContactBook from "@/views/ContactBook.vue";
-import { createRouter, createWebHistory } from "vue-router";
-
+import ContactBook from '@/views/ContactBook.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
     {
-        path: "/",
-        name: "contactbook",
+        path: '/',
+        name: 'contactbook',
         component: ContactBook,
     },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'notfound',
+        component: () => import('@/views/NotFound.vue'),
+    },
+    {
+        path: '/contacts/:id',
+        name: 'contact.edit',
+        component: () => import('@/views/ContactEdit.vue'),
+        props: true,
+    },
+    {
+        path: '/contacts/add',
+        name: 'contact.add',
+        component: () => import('@/views/ContactAdd.vue'),
+        props: true,
+    },
 ];
-
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
-
 export default router;
